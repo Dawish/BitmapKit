93850fb2ac0f668573081959def00a9daa593af5
742637254@qq.com <dxx13617237755>
2018-01-14 13:49:26 +0800

    :fire: 花样儿试实现图片滤镜

--- app/src/main/java/com/danxx/MainActivity.java
+++ app/src/main/java/com/danxx/MainActivity.java
@@ -1,9 +1,14 @@
 package com.danxx;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import com.danxx.utils.BlurKit;
 
@@ -12,12 +17,36 @@ import com.danxx.utils.BlurKit;
  */
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         BlurKit.init(MainActivity.this);
 
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                Button touchedButton = (Button)view;
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        touchedButton.getBackground().setColorFilter(0x22000000, PorterDuff.Mode.SRC_ATOP);
+                        touchedButton.invalidate();
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_UP:
+                        touchedButton.getBackground().clearColorFilter();
+                        touchedButton.invalidate();
+                        break;
+                }
+                return true;
+
+            }
+        });
 
     }
 }
