69b875b6a926932b5e62bc8a87416b476c4f2f60
742637254@qq.com <dxx13617237755>
2018-01-25 21:02:40 +0800

    :fire: add crop demo

---
+++ app/src/main/java/com/danxx/ActivityCrop.java
@@ -0,0 +1,13 @@
+package com.danxx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityCrop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop);
+    }
+}
